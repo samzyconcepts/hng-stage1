c68149ee754d7c0ed8e2e030ee992c9ab6241c19
samzyconcepts <samson.oderinwale@kibo.school>
2024-07-07 01:35:54 +0100

    update the style and javascript

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function updateTimeAndDay() {
-    const currentTimeUTC = new Date().toUTCString();
+    const currentTimeUTC = new Date().toUTCString().split(" ")[4];
     const currentDay = new Date().toLocaleString("en-us", { weekday: "long" });
-    document.querySelector('[data-testid="currentTimeUTC"]').textContent =
-      currentTimeUTC;
+    document.querySelector('[data-testid="currentTimeUTC"]').textContent = currentTimeUTC;
     document.querySelector('[data-testid="currentDay"]').textContent = currentDay;
-  }
-  
-  updateTimeAndDay();
-  setInterval(updateTimeAndDay, 1000);
+}
+
+updateTimeAndDay();
+setInterval(updateTimeAndDay, 1000);
